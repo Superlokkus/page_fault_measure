72303c5d4641b4ee06d97849891f4193f6f1d8f3
Markus Klemm <markus@markusklemm.net>
2015-10-26 11:54:40 +0100

    Source directory and generated header file with macros works

--- src/pagefault_measure.c
+++ src/pagefault_measure.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include "page_fault_measure.h"
 
 int main(int argc, char** args)
 {
-    printf("Jup\n");
+    printf("pagefaulter %d\n",PAGE_FAULT_MEASURE_VERSION_MINOR);
     return 0;
 }
