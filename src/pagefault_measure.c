77f023cdb504c4bcbd5ff16fe7b427df436db680
Markus Klemm <markus@markusklemm.net>
2015-10-26 13:30:05 +0100

    Some renaming took place

--- src/pagefault_measure.c
+++ src/pagefault_measure.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include "page_fault_measure.h"
+#include "pagefault_measure.h"
 
 int main(int argc, char** args)
 {
-    printf("pagefaulter %d\n",PAGE_FAULT_MEASURE_VERSION_MINOR);
+    printf("pagefaulter %d\n",PAGEFAULT_MEASURE_VERSION_MINOR);
     return 0;
 }
