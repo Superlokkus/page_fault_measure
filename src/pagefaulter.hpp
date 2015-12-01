9ca724b599d3aef472432019a49210734c42bf4f
Markus Klemm <markus@markusklemm.net>
2015-12-01 13:20:49 +0100

    Reorganized the source file handling for cmake and added a barebone pagefaulter class

--- src/pagefaulter.hpp
+++ src/pagefaulter.hpp
@@ -1,9 +1,30 @@
 /*! @file pagefaulter.hpp
  */
 
-#ifndef pagefaulter_hpp
-#define pagefaulter_hpp
+#ifndef PAGEFAULT_MEASURE_SRC_PAGEFAULTER_HPP
+#define PAGEFAULT_MEASURE_SRC_PAGEFAULTER_HPP
 
+#include <cstddef>
+
+namespace pagefault_measure {
+    
+    /*! @brief Class to provoke and measure the time duration of a pagefault
+     */
+    class pagefaulter
+    {
+        
+        pagefaulter ()
+        {
+            
+        }
+        
+        pagefaulter (size_t memory_size_mib){
+            
+        }
+        
+    };
+    
+}
 
 
 #endif /* pagefaulter_hpp */
